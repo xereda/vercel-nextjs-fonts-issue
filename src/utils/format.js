38826f68c5ba7e14f243fc58377937e33107d266
Jackson RIcardo Schroeder <jacksonrs@gmail.com>
2022-02-09 16:40:11 -0300

    feat: add assets to login page

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,12 @@
-export const toCPFMask = cpf => cpf;
+import { validate } from 'gerador-validador-cpf';
 
-export const isValidCPF = cpf => !!cpf;
+export const toCPFMask = cpf => {
+  return cpf
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const isValidCPF = validate;
