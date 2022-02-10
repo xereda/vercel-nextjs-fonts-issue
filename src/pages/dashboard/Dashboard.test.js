eced3fc224a7736d671aa470525eee6d59ab475f
Jackson RIcardo Schroeder <jacksonrs@gmail.com>
2022-02-10 12:39:04 -0300

    fix: general adjustments

--- src/pages/dashboard/Dashboard.test.js
+++ src/pages/dashboard/Dashboard.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import Dashboard from './Dashboard';
 import { rest } from 'msw';
 import { server } from '@/mocks/server';
