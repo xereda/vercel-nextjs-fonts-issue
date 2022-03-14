53f5c799a1e52ed1bf0adfddf124152484f02a08
Jackson RIcardo Schroeder <jacksonrs@gmail.com>
2022-03-14 10:38:33 -0300

    chore: add environment files

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,10 +1,16 @@
-if (process.env.NODE_ENV === 'development') {
+console.log('### process.env ###: ', process.env.NEXT_PUBLIC_ENVIRONMENT);
+
+if (
+  process.env.NODE_ENV === 'development' &&
+  !process.env.NEXT_PUBLIC_ENVIRONMENT
+) {
   if (typeof window === 'undefined') {
     const { server } = require('./server');
 
     server.listen();
   } else {
     const { worker } = require('./browser');
+    console.log('vai habilitar o MSW');
 
     worker.start({
       onUnhandledRequest: 'bypass',
