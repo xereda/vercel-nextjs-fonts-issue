6cbeebb6cc0c0ad0d6816abf89839a9f7cad3efb
Jackson RIcardo Schroeder <jacksonrs@gmail.com>
2022-03-14 18:39:16 -0300

    chore: add module for cross platform unix cp command

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  env: {
-    HEIMDALL_CLIENT: process.env.HEIMDALL_CLIENT,
-    AUTHENTICATION_PATH: process.env.AUTHENTICATION_PATH,
-  },
   async redirects() {
     return [
       {
