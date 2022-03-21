065439e00fd6792eeb0b311b7c6898fe6ccca9be
Jackson RIcardo Schroeder <jacksonrs@gmail.com>
2022-03-21 08:58:53 -0300

    fix: change test matcher

--- src/modules/login/Login.test.js
+++ src/modules/login/Login.test.js
@@ -24,7 +24,9 @@ describe('Form login component', () => {
     await userEvent.type(cpf, '11651232903{tab}');
     await userEvent.type(password, '1{tab}');
 
-    expect(screen.queryByText(/cpf digitado não é valido/i)).toBeNull();
+    expect(
+      screen.queryByText(/cpf digitado não é valido/i),
+    ).not.toBeInTheDocument();
     expect(screen.queryAllByText(/campo obrigatório/i)).toHaveLength(0);
     expect(screen.getByRole('button', { name: 'Fazer login' })).toBeEnabled();
     const button = screen.getByRole('button', { name: 'Fazer login' });
