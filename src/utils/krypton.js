30827acac03deb5eb6225e0302ac35715ed26b80
Jackson RIcardo Schroeder <jacksonrs@gmail.com>
2022-03-21 09:02:37 -0300

    fix: rename argument function

--- src/utils/krypton.js
+++ src/utils/krypton.js
@@ -155,8 +155,8 @@ export const makeKrypton = () => {
 
   const decrypt = (responseData) => kryptonInstance.decrypt(responseData);
 
-  const generateHash = (kryptonPublicKey, timestamep) =>
-    kryptonInstance.generateHash(kryptonPublicKey, timestamep);
+  const generateHash = (kryptonPublicKey, timestamp) =>
+    kryptonInstance.generateHash(kryptonPublicKey, timestamp);
 
   return {
     encrypt,
