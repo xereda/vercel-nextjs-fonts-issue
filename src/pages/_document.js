c0a44b3e41c69bf7d86e639a78318357077fb05a
Laiz Lira <laizgamalira@gmail.com>
2022-05-30 14:04:51 -0300

    feat: adjust _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,35 +1,26 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getStaticProps(ctx) {
-    const initialProps = await Document.getStaticProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" type="image/svg" href="/svg/icon-ben.svg" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;600;700&family=Open+Sans:wght@300;400;600;700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <div id="myportal" />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" type="image/svg" href="/svg/icon-ben.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;600;700&family=Open+Sans:wght@300;400;600;700&display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <div id="myportal" />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
